c41c9a13c8cf97dd24fd1b8734b983ca1417aa14
songweiren <fnr.song@aliyun.com>
2020-12-28 20:54:21 +0800

    mod domin

--- sw.js
+++ sw.js
@@ -31,9 +31,9 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "sfnr.top",
+  "songweiren.githu.io",
+  "songweiren.top"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
